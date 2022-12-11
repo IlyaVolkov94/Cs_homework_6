23e8e603404c07cc51ad3b5620056004519e0d0e
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-11 13:14:34 +0300

    task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа");
+string text = Convert.ToString(Console.ReadLine());
+string[] numbersString = text.Split(' ', ';');
+int[] numbersInt = new int[numbersString.Length];
+int count=0;
+for(int i=0;i<numbersString.Length;i++)
+    {
+        numbersInt[i]=Convert.ToInt32(numbersString[i]);
+        if(numbersInt[i]>0)
+            count++;
+    }
+Console.WriteLine(count);
