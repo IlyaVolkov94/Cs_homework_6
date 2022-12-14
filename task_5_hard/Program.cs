b61b5ce0d1bbd28ff6ad1d3a6a527ae5e091f7f0
Edu_Only <edu@cation.ru>
2022-12-14 21:28:49 +0300

    task5_in_progress

--- task_5_hard/Program.cs
+++ task_5_hard/Program.cs
@@ -5,24 +5,11 @@
 Одно число - это не последовательность.
 */
 
-int[] FillRandomArray(int value)
+int[] FillArray(int value)
 {
     int[] array = new int[value];
-    int temp = new Random().Next(0, value * 2);
-    int j = 0;
     for (int i = 0; i < value; i++)
-    {
-        while (j < i)   //Проверяем рандомное число на наличие его в массиве
-        {
-            if (array[j] == temp)
-            {
-                temp = new Random().Next(0, value);
-                j = 0;
-            }
-            else j++;
-        }
-        array[i] = temp;
-    }
+        array[i] = new Random().Next(-value, value);
     return array;
 }
 
@@ -49,9 +36,29 @@ void CheckSeries(int[] array, int[] MaxAndMinArray)
     int indexMax = 0;
     int indexMin = 1;
     int i = MaxAndMinArray[indexMin];
-    while(i<=MaxAndMinArray[indexMax])
+    int count = -1;
+    int j = 0;
+    int firstNumSeries = 0;
+    int series=0;
+    while (i <= MaxAndMinArray[indexMax])
     {
-        
+        while (j < array.Length)
+        {
+            if (i == array[j])
+            {
+                count++;
+                break;
+            }
+        }
+        if (j == array.Length)
+        {
+            firstNumSeries=i;
+            series=count;
+            count=0;
+            i++;
+        }
+        i++;
+        j = 0;
     }
 
 }
