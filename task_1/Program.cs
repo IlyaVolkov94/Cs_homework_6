b082d980709ff3b69c4bea21247048dbae48914e
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-15 21:26:08 +0300

    task_1_edit

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,14 +1,21 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] TransformStringtoInt(string text)
+{
+    string[] stringSeparators = new string[] { ", ", "; ", " " };
+    string[] numbersString = text.Split(stringSeparators, StringSplitOptions.None);
+    int[] numbersInt = new int[numbersString.Length];
+    for (int i = 0; i < numbersString.Length; i++)
+        numbersInt[i] = Convert.ToInt32(numbersString[i]);
+    return numbersInt;
+}
+
 Console.WriteLine("Введите числа");
 string text = Convert.ToString(Console.ReadLine())!;
-string[] spltStrings = new string[] {", ", "; ", " "};
-string[] numbersString = text.Split();
-int[] numbersInt = new int[numbersString.Length];
-int count=0;
-for(int i=0;i<numbersString.Length;i++)
-    {
-        numbersInt[i]=Convert.ToInt32(numbersString[i]);
-        if(numbersInt[i]>0)
-            count++;
-    }
+int[] array = TransformStringtoInt(text);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
 Console.WriteLine(count);
